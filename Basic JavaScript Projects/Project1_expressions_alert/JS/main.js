7d9cc53a5a306a6be361af5f7bd78a45a80f83b0
ChrisHu79 <chrishumble.uk@outlook.com>
2022-10-15 14:32:27 +0100

    Javascript assignment 1
    This is my first attempt at writing Javascript.

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,24 @@
+var A = "This is a string for assignment number 7";
+window.alert(A);
+
+document.write("\"This is a concatenated string,"
++"there is alot of work involved in Javascript, it isn\'t the easiest thing to learn\"")
+
+var B = "Java"+ "script is actually quite fun once you understand it a bit."
+
+document.write(B);
+
+var Family = "The Humbles", Dad = "Chris", Mum = "Sam", Daughter = "Sarah", Son = "Christian";
+var Dad = Dad.fontcolor("blue");
+var Mum = Mum.fontcolor("green");
+var Daughter = Daughter.fontcolor("purple");
+var Son = Son.fontcolor("red");
+var Family = Family.fontcolor("yellow");
+
+document.write(Dad);
+document.write(Mum);
+document.write(Daughter);
+document.write(Son);
+document.write(Family);
+
+document.write(2*3)
