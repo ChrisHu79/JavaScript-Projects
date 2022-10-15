68c0f540fa9588103285dd7b651e047ccbab185d
ChrisHu79 <chrishumble.uk@outlook.com>
2022-10-15 14:40:56 +0100

    Update main.js
    Updates

--- Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -1,24 +1,24 @@
-var A = "This is a string for assignment number 7";
-window.alert(A);
+var A = "This is a string for assignment number 7";//Defining a variable and giving it a string value
+window.alert(A);//Creates a pop up window containing the string
 
-document.write("\"This is a concatenated string,"
+document.write("\"This is a concatenated string,"//Prints the specified text to the page
 +"there is alot of work involved in Javascript, it isn\'t the easiest thing to learn\"")
 
-var B = "Java"+ "script is actually quite fun once you understand it a bit."
+var B = "Java"+ "script is actually quite fun once you understand it a bit."//Defining a variable and giving it a string value
 
-document.write(B);
+document.write(B);//Prints the specified text to the page
 
-var Family = "The Humbles", Dad = "Chris", Mum = "Sam", Daughter = "Sarah", Son = "Christian";
-var Dad = Dad.fontcolor("blue");
-var Mum = Mum.fontcolor("green");
-var Daughter = Daughter.fontcolor("purple");
-var Son = Son.fontcolor("red");
-var Family = Family.fontcolor("yellow");
+var Family = "The Humbles", Dad = "Chris", Mum = "Sam", Daughter = "Sarah", Son = "Christian";//Defining a variable and giving it a string value
+var Dad = Dad.fontcolor("blue");//Defines the font colour for the selected variable
+var Mum = Mum.fontcolor("green");//Defines the font colour for the selected variable
+var Daughter = Daughter.fontcolor("purple");//Defines the font colour for the selected variable
+var Son = Son.fontcolor("red");//Defines the font colour for the selected variable
+var Family = Family.fontcolor("yellow");//Defines the font colour for the selected variable
 
-document.write(Dad);
-document.write(Mum);
-document.write(Daughter);
-document.write(Son);
-document.write(Family);
+document.write(Dad);//Prints the specified text to the page
+document.write(Mum);//Prints the specified text to the page
+document.write(Daughter);//Prints the specified text to the page
+document.write(Son);//Prints the specified text to the page
+document.write(Family);//Prints the specified text to the page
 
-document.write(2*3)
+document.write(2*3)//prints the result of the specified function
